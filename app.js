6383703c801fa2a4ec1e877988384e8e7b2575e2
Paulth25 <paulthughes024@gmail.com>
2022-06-23 16:56:17 +0100

    unfinished dice game

---
+++ app.js
@@ -0,0 +1,9 @@
+const myheading = document.getElementById("heading")
+
+const input = documnet.getElementById("input")
+
+const button = document.getElementById("button")
+
+button.addEventListener("click", ()=>{
+    heading.style.color = input.value
+})
