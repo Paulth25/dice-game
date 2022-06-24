090d118fddfcca05ec13644d8be1425239466e9b
Paulth25 <104359183+Paulth25@users.noreply.github.com>
2022-06-24 15:20:22 +0100

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-const myheading = document.getElementById("heading")
-
-const input = documnet.getElementById("input")
-
-const button = document.getElementById("button")
-
-button.addEventListener("click", ()=>{
-    heading.style.color = input.value
-})
